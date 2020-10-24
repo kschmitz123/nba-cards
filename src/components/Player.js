b9e67bdc74d2ee86e3787deb33adb88043875f01
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-10-24 16:42:19 +0200

    Add player styling

--- src/components/Player.js
+++ src/components/Player.js
@@ -5,13 +5,13 @@ const Name = Component("h3", {
   innerText: "Al Horford",
   className: "player__name",
 });
-const Infos = Component("p", { innerText: "position" });
+const Infos = Component("p");
 const InfoContainer = Component("div", {
   className: "player__infos",
   children: [
-    Infos({ className: "player__position" }),
-    Infos({ className: "player__team" }),
-    Infos({ className: "player__city" }),
+    Infos({ innerText: "Position: ", className: "player__position" }),
+    Infos({ innerText: " Team", className: "player__team" }),
+    Infos({ innerText: "city", className: "player__city" }),
   ],
 });
 const Player = Component("article", {
