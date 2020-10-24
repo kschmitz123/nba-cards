164d83c592f08fdb1dd65404ef01f4eae686f9ce
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-10-24 17:50:35 +0200

    Get API running

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import "./app.css";
 import AppBar from "./components/AppBar";
-import Player from "./components/Player";
-import { Component } from "./utils/elements";
+import createPlayer from "./components/Player";
+import { createElement } from "./utils/elements";
+import { getAllPlayers } from "./utils/api";
 
-const Main = Component("main");
-const Container = Component("div");
-
-const App = () =>
-  Container({
-    children: [AppBar(), Main(), Player()],
+function App() {
+  const main = createElement("main", {
+    className: "main",
   });
 
+  async function loadPlayers(name) {
+    const players = await getAllPlayers(name);
+    console.log(players);
+    const playerElements = players.map((player) =>
+      createPlayer({
+        name: player.first_name,
+        position: player.position,
+        team: player.team,
+        city: player.team,
+      })
+    );
+    main.append(...playerElements);
+  }
+
+  loadPlayers();
+
+  const container = createElement("div", {
+    children: [AppBar(), main],
+  });
+  return container;
+  //   const App = () =>
+  //     Container({
+  //       children: [AppBar(), Main()],
+  //     });
+}
 export default App;
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,9 @@
-export async function getALlPlayers(name) {
-  let url = `https://www.balldontlie.io/api/v1/players`;
+export async function getAllPlayers(name) {
+  let url = `https://www.balldontlie.io/api/v1/players/`;
   if (name) {
     url += `?search=${name}`;
   }
   const response = await fetch(url);
-  const data = await response.json();
-  return data;
+  const results = await response.json();
+  return results.data;
 }
