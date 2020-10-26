bd80088df2832d08351a80957e3aaf87bd709121
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-10-26 13:01:59 +0100

    Add reload link to header

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -2,20 +2,27 @@ import "./appBar.css";
 import logoSrc from "../assets/nba-logo.png";
 import { Component } from "../utils/elements";
 
+const Link = Component("a", {
+  onclick: () => window.location.reload(),
+});
 const HeaderText = Component("h1");
 const Logo = Component("img");
 const Header = Component("header", {
   className: "header",
 });
 const AppBar = () =>
-  Header({
+  Link({
     children: [
-      Logo({
-        src: logoSrc,
-        alt: "Logo",
-      }),
-      HeaderText({
-        innerText: "NBA Player Stats",
+      Header({
+        children: [
+          Logo({
+            src: logoSrc,
+            alt: "Logo",
+          }),
+          HeaderText({
+            innerText: "NBA Player Stats",
+          }),
+        ],
       }),
     ],
   });
